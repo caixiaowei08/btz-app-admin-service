21cfec701aa6a95795ffe69a2a0ffa8ec4305132
caixiaowei <caixiaowei@caixiaoweideMacBook-Pro.local>
2018-05-21 01:12:55 +0800

    20180521-fix-caroural

--- src/main/java/app/btz/function/carousel/controller/AppCarouselController.java
+++ src/main/java/app/btz/function/carousel/controller/AppCarouselController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,9 +36,35 @@ public class AppCarouselController extends BaseController {
     @Autowired
     private CarouselService carouselService;
 
+    /**
+     * 1、【过时 由于实现时 图片数据从数据库拉取 数据较大 性能问题】由getCarouselForFix替代
+     * 2、为了维护老版本app 则保留该接口
+     * @param carouselEntity
+     * @param request
+     * @param response
+     * @return
+     */
+    @Deprecated
     @RequestMapping(params = "getCarousel")
     @ResponseBody
     public AppAjax getCarousel(CarouselEntity carouselEntity, HttpServletRequest request, HttpServletResponse response) {
+        AppAjax j = new AppAjax();
+        List<CarouselEntity> carouselEntityList = new ArrayList<CarouselEntity>();
+        j.setContent(carouselEntityList);
+        return j;
+    }
+
+
+    /**
+     * 优化版本 20180518
+     * @param carouselEntity
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(params = "getCarouselForNewVersion")
+    @ResponseBody
+    public AppAjax getCarouselForNewVersion(CarouselEntity carouselEntity, HttpServletRequest request, HttpServletResponse response) {
         AppAjax j = new AppAjax();
         DetachedCriteria carouselDetachedCriteria = DetachedCriteria.forClass(CarouselEntity.class);
         carouselDetachedCriteria.add(Restrictions.eq("sfyn", SfynConstant.SFYN_Y));
